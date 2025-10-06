d8e76cb0bc7adb69cbbf47ba3f40a81016de9a96
WiHceeg <luo_s_q@outlook.com>
2025-10-07 03:03:03 +0800

    delete course

--- tutor-web-app-ssr/static/courses.js
+++ tutor-web-app-ssr/static/courses.js
@@ -113,3 +113,38 @@ updateCourseForm.addEventListener('submit', function (event) {
         })
         .catch(error => console.error('Error:', error));
 });
+
+
+
+const deleteCourseForm = document.getElementById('DeleteCourse');
+
+deleteCourseForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // 阻止浏览器默认动作（即不向服务器发送请求）
+
+    let course_id = document.getElementById('delete_course_id').value;
+    console.log(course_id);
+
+    const tutor_id = getCookie('tutor_id');
+    if (!tutor_id) {
+        console.error('No tutor_id found in cookies');
+        alert('tutor_id not found in cookies. Please log in.');
+        return;
+    }
+
+    fetch(`/courses/delete/${tutor_id}/${course_id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response;
+        })
+        .then(data => {
+            if (confirm(`Delete course success, Click Confirm to refresh the page and display.`)) {
+                location.reload();   // 按了「确定」就刷新
+            }
+        })
+        .catch(error => console.error('Error:', error));
+});
